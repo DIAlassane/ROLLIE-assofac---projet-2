24e9f54d1a007fd2b86c23bd42a2f261eb936ac5
DiAlassane <owner@alassane-2.local>
2023-06-13 10:06:29 +0200

    daytona

--- daytona/rolex.js
+++ daytona/rolex.js
@@ -48,3 +48,20 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+// ACORDEON - 5 --------------------------------------
+
+var acc = document.getElementsByClassName("accordion5");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
