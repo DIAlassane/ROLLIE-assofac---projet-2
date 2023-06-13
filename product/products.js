80fc28bb95e3c0c61dee90b80c62269490c62903
DIAlassane <132441754+DIAlassane@users.noreply.github.com>
2023-06-13 22:40:11 +0200

    filtre

--- product/products.js
+++ product/products.js
@@ -47,3 +47,57 @@ setInterval(()=>{
     }
 
 },3000)
+
+// ACORDEON - 667 --------------------------------------
+
+var acc = document.getElementsByClassName("accordion667");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+// FILTRE 667 ------------------ --------------------------- ----------------------- ----------------
+
+filterObjects("tout");
+
+function filterObjects(c){
+  var x, i;
+  x = document.getElementsByClassName("ggrid");
+  if (c == "tout") c = "";
+  for (i = 0; i < x.length; i++) {
+    removeClass(x[i], "show");
+    if(x[i].className.indexOf(c) > -1) addClass(x[i], "show")
+  }
+}
+
+function addClass(element, name){
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++){
+    if (arr1.indexOf(arr2[i]) == -1){
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+function removeClass(element, name){
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++){
+    while (arr1.indexOf(arr2[i]) > -1){
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
